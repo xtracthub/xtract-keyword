59b05c6cffa2bce39b55a410adaf08ff2e5a8932
Ryan Wong <rewong03@uchicago.edu>
2019-08-07 23:21:49 -0500

    Fixed to work with automated training in xtract-sampler

--- xtract_keyword_main.py
+++ xtract_keyword_main.py
@@ -8,10 +8,6 @@ import argparse
 from rake_nltk import Rake
 
 
-with open('words_dictionary.json', 'r') as words_file:
-    dict_of_words = json.load(words_file)
-
-
 def read_files(file):
     """Reads a file and returns a string of contents.
 
@@ -58,6 +54,8 @@ def extract_keyword(file_path, top_n=20):
     stop_words = ['\n']
     with open(os.getcwd() + '/stop-words-en.txt', 'r') as f:
         stop_words += [x.strip() for x in f.readlines()]
+    with open('words_dictionary.json', 'r') as words_file:
+        dict_of_words = json.load(words_file)
 
     if file_path.endswith('.pdf'):
         docs = pdf_to_text(file_path)
